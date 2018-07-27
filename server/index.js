3c20edb3cc89f44bd851333073bad3f4de7465da
SAPINFO\ben.liao <ben.liao@sapphireinfo.com.tw>
2018-07-27 18:51:59 +0800

    add server for mypage

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import path from 'path'
+import morgan from 'morgan'
+import router from './router'
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '..', '/client/build')))
+
+// log all request details
+app.use(morgan('dev'))
+
+app.use('/api', router)
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client/build/index.html'))
+})
+
+
+
+app.listen(PORT, () => console.log(`Express server is running on port ${PORT}`))
---
+++ server/router.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import axios from 'axios'
+
+const router = express.Router()
+const PTT_SERVICE = process.env.PTT_SERVICE || "http://localhost:8888/"
+
+// router.use((req, res, next) => {
+//     console.log("Calling API at: " + Date.now())
+//     next()
+// })
+
+router.get('/hotboards', (req, res) => {
+    axios.get(PTT_SERVICE)
+        .then(({data}) => {
+            console.log(data)                  
+            res.send(data)
+        })
+        .catch(console.log)
+})
+
+router.get('/posts/:count', (req, res) => {
+    const {count} = req.params
+    const {url} = req.query
+    axios.get(`${PTT_SERVICE}list?url=${url}&count=${count}`)
+        .then(({data}) => {
+            console.log(data)                  
+            res.send(data)
+        })
+        .catch(console.log)
+})
+
+router.get('/post', (req, res) => {
+    const {url} = req.query
+    axios.get(`${PTT_SERVICE}post?url=${url}`)
+        .then(({data}) => {            
+            data.content = data.content.replace(/(\n)/gm, '<br />')
+            console.log(data) 
+                             
+            res.send(data)
+        })
+        .catch(console.log)
+})
+
+export default router
